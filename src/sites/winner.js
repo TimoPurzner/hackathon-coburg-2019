88e2a7587e130e798108f086703b53fad095aea9
ZeroSoulEater <t1@purzner.de>
2019-11-21 02:02:19 +0100

    add routing

---
+++ src/sites/winner.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+
+class Winner extends Component {
+
+    render() {
+        return (
+            <div className=''>
+                <h1>Gewinner der Herzen 2018</h1>
+                <img src="/images/so_sehen_sieger_aus.jpg" 
+                    alt="Sieger der Herzen"
+                />
+            </div>
+        )
+    }
+}
+
+export default Winner;
